2d2f7a4a5eaffd2f1ac217572f02f68a3c684c14
Jenny Chen <jenny.chenchou@gmail.com>
2023-06-11 12:10:19 +0200

    Add user registration functionality and API endpoint

--- clone_airbnb/src/App.jsx
+++ clone_airbnb/src/App.jsx
@@ -2,13 +2,18 @@ import "./App.css";
 import {Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage"
+import axios from "axios"
 
+axios.defaults.baseURL = 'http://localhost:8000'
 
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
     </Routes>
   );
 }
--- clone_airbnb/src/pages/LoginPage.jsx
+++ clone_airbnb/src/pages/LoginPage.jsx
@@ -1,19 +1,25 @@
 import Header from "../components/Header";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   return (
-    <div className="mt-4 ">
+    <div className="p-4 flex flex-col min-h-screen">
       <Header />
-      <div className="mt-20">
-      <h1 className="text-3xl text-center mb-4">Login</h1>
-      <form className="max-w-lg mx-auto">
-        <input type="email" placeholder="example@gmail.com" />
-        <input type="password" placeholder="password" />
-        <button className="buttonRed">Login</button>
-        <div className="text-center py-2 text-gray-500">Don't have an account yet? &nbsp;
-        <Link className="underline text-black" to={'/register'}>Register now</Link></div>
-      </form>
+      <div className="mt-4 grow flex items-center justify-center">
+        <div className="mb-64">
+          <h1 className="text-3xl text-center mb-4">Login</h1>
+          <form className="max-w-lg mx-auto">
+            <input type="email" placeholder="example@gmail.com" />
+            <input type="password" placeholder="password" />
+            <button className="buttonRed">Login</button>
+            <div className="text-center py-2 text-gray-500">
+              Don't have an account yet? &nbsp;
+              <Link className="underline text-black" to={"/register"}>
+                Register now
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
