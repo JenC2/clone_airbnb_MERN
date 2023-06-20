62ad8098850ebc6b40e0a53b37c305c0fab3e55d
Jenny Chen <jenny.chenchou@gmail.com>
2023-06-21 00:37:59 +0200

    refactoring: sort all the svg to svg folder

--- clone_airbnb/src/components/Header.jsx
+++ clone_airbnb/src/components/Header.jsx
@@ -1,4 +1,7 @@
 import logo from "../assets/logo.png";
+import searchIcon from "../assets/svg/searchIcon.svg";
+import menuLineIcon from "../assets/svg/menuLineIcon.svg";
+import userIcon from "../assets/svg/userIcon.svg";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../UserContext";
@@ -18,54 +21,17 @@ export default function Header() {
           <div>Any week</div>
           <div className="border-l border-grey-300"></div>
           <div>Add guests</div>
-          <button className="bg-mainRed text-white p-2 rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-4 h-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
+          <button className="bg-mainRed p-2 rounded-full">
+            <img src={searchIcon} alt="" className="w-4 h-4" />
           </button>
         </div>
         <Link
           to={user ? "/account" : "/login"}
           className="flex border border-grey-300 rounded-full py-2 px-4 gap-3"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 relative top-1"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-          <div className="bg-gray-500 text-white rounded-full p-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 2 24 24"
-              fill="currentColor"
-              className="w-6 h-6 relative top-1"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                clipRule="evenodd"
-              />
-            </svg>
+          <img src={menuLineIcon} alt="menuLineIcon" className="w-6 h-6 relative top-1" />
+          <div className="bg-gray-500 rounded-full p-1">
+            <img src={userIcon} alt="userIcon" className="w-6 h-6 relative top-1" />
           </div>
           {Boolean(user) ? <div>{user.name}</div> : null}
         </Link>
--- clone_airbnb/src/pages/AccountPlacePage.jsx
+++ clone_airbnb/src/pages/AccountPlacePage.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { UserContext } from "../UserContext";
 import { Link, Navigate } from "react-router-dom";
 import MyAccomodations from "../components/MyAccomodations";
+import plusIcon from "../assets/svg/plusIcon.svg";
+
 
 export default function AccountPlacePage() {
   const { ready, user } = useContext(UserContext);
@@ -23,20 +25,7 @@ export default function AccountPlacePage() {
             className="inline-flex gap-1 bg-mainRed text-white py-2 px-6 rounded-full"
             to={"/account/places/new"}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
+            <img src={plusIcon} alt="" className="w-6 h-6" />
             Add new place
           </Link>
         </div>
