98ac91d1c77e3e5ecc2f1f9139e6375074e37a60
Jenny Chen <jenny.chenchou@gmail.com>
2023-07-11 16:27:24 +0200

    add function for the features checkedbox

--- clone_airbnb/src/components/Features.jsx
+++ clone_airbnb/src/components/Features.jsx
@@ -5,30 +5,39 @@ import poolIcon from "../assets/svg/poolIcon.svg";
 import kitchenIcon from "../assets/svg/kitchenIcon.svg";
 
 export default function Features({selected, onChange}) {
+  const handleCheckbox = (ev) => {
+    const {checked, name} = ev.target;
+    // console.log(ev.target.name);
+    if (checked) {
+      onChange([...selected, name]);
+    } else {
+      onChange([...selected.filter(selectedName => selectedName !== name)])
+    }
+  }
   return (
     <>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" />
+        <input type="checkbox" name="wifi" onChange={handleCheckbox} />
         <img src={wifiIcon} alt="wifiIcon" className="w-6 h-6" />
         <span>Wifi</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" />
+        <input type="checkbox" name="parking" onChange={handleCheckbox} />
         <img src={parkingIcon} alt="parkingIcon" className="w-6 h-6" />
         <span>Free parking</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" />
+        <input type="checkbox" name="tv" onChange={handleCheckbox} />
         <img src={tvIcon} alt="tvIcon" className="w-6 h-6" />
         <span>TV</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" />
+        <input type="checkbox" name="pool" onChange={handleCheckbox} />
         <img src={poolIcon} alt="poolIcon" className="w-6 h-6" />
         <span>Private pool</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" />
+        <input type="checkbox" name="kitchen" onChange={handleCheckbox} />
         <img src={kitchenIcon} alt="kitchenIcon" className="w-6 h-6" />
         <span>Kitchen</span>
       </label>
