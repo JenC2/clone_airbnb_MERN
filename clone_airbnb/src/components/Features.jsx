27879f8bc936704e7435bd9c20552414f0f9c780
Jenny Chen <jenny.chenchou@gmail.com>
2023-07-18 18:38:08 +0200

    add checked attribute in Features

--- clone_airbnb/src/components/Features.jsx
+++ clone_airbnb/src/components/Features.jsx
@@ -4,7 +4,7 @@ import tvIcon from "../assets/svg/tvIcon.svg";
 import poolIcon from "../assets/svg/poolIcon.svg";
 import kitchenIcon from "../assets/svg/kitchenIcon.svg";
 
-const Features = ({ selected, onChange }) => {
+export default function Features ({ selected, onChange }) {
   function handleCheckbox(ev) {
     const { checked, name } = ev.target;
     // console.log(ev.target.name);
@@ -17,31 +17,30 @@ const Features = ({ selected, onChange }) => {
   return (
     <>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" name="wifi" onChange={handleCheckbox} />
+        <input type="checkbox" checked={selected.includes("wifi")} name="wifi" onChange={handleCheckbox} />
         <img src={wifiIcon} alt="wifiIcon" className="w-6 h-6" />
         <span>Wifi</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" name="parking" onChange={handleCheckbox} />
+        <input type="checkbox" checked={selected.includes("parking")} name="parking" onChange={handleCheckbox} />
         <img src={parkingIcon} alt="parkingIcon" className="w-6 h-6" />
         <span>Free parking</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" name="tv" onChange={handleCheckbox} />
+        <input type="checkbox" checked={selected.includes("tv")} name="tv" onChange={handleCheckbox} />
         <img src={tvIcon} alt="tvIcon" className="w-6 h-6" />
         <span>TV</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" name="pool" onChange={handleCheckbox} />
+        <input type="checkbox" checked={selected.includes("pool")} name="pool" onChange={handleCheckbox} />
         <img src={poolIcon} alt="poolIcon" className="w-6 h-6" />
         <span>Private pool</span>
       </label>
       <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-        <input type="checkbox" name="kitchen" onChange={handleCheckbox} />
+        <input type="checkbox" checked={selected.includes("kitchen")} name="kitchen" onChange={handleCheckbox} />
         <img src={kitchenIcon} alt="kitchenIcon" className="w-6 h-6" />
         <span>Kitchen</span>
       </label>
     </>
   );
 };
-export default Features;
