cb74138c3477086aac9fc6f4b6f312d61b701111
Jenny Chen <jenny.chenchou@gmail.com>
2023-08-04 16:45:58 +0200

    add default booking date

--- clone_airbnb/src/components/BookingWidget.jsx
+++ clone_airbnb/src/components/BookingWidget.jsx
@@ -1,7 +1,15 @@
 
 export default function BookingWidget({place}) {
+
+  //toISOString() method converts the date into format "YYYY-MM-DDTHH:mm:ss.sssZ"
+  const currentDate = new Date().toISOString().split('T')[0];
+
+  const checkOutDate = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000)
+    .toISOString()
+    .split('T')[0];
+
   return (
-    <div className="bg-white shadow p-4 rounded-2xl">
+    <div className="bg-white border border-gray-200 shadow-md shadow-gray-300 p-4 rounded-2xl">
       <div className="text-2xl text-center">
         Price: €{place.price} / per night
       </div>
@@ -9,11 +17,11 @@ export default function BookingWidget({place}) {
         <div className="flex">
           <div className="py-3 px-4">
             <label>CHECK-IN</label>
-            <input type="date" />
+            <input type="date" defaultValue={currentDate} />
           </div>
           <div className="py-3 px-4 border-l">
             <label>CHECKOUT</label>
-            <input type="date" />
+            <input type="date" defaultValue={checkOutDate}/>
           </div>
         </div>
         <div>
